7cf0c79f447d6932b7caa8c5175f603c3af0eed4
Jiayu Gao <GalaxyJiayu@gmail.com>
2018-10-14 13:27:26 -0600

    Manager js change

--- Manager/Manager.js
+++ Manager/Manager.js
@@ -4,7 +4,7 @@ let dba = require("./modules/ManagerDashboard.js");
 
 app.get('/ManagerDashboard', async function (req, res) {
     let result;
-    await dba.addGroup("Max","Test");
+    //await dba.addGroup("Max","Test");
     result=await dba.queryDisplayGroups(req.session.username);
     res.send(result);
 });
